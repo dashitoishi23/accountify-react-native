1c211deda3a82ead405b7cb8967f99673bbdab02
Hitoishi Das <hitoishi.das@gmail.com>
2023-01-23 18:25:58 +0530

    Added splash screen

--- components/HomeScreen.tsx
+++ components/HomeScreen.tsx
@@ -1,10 +1,13 @@
 import React, {useEffect} from 'react';
 import {Text} from 'react-native';
 import {dbOps} from '../util/db';
+import SplashScreen from 'react-native-splash-screen';
 
 const HomeScreen = () => {
   useEffect(() => {
     let dbInfo = dbOps.initiateDBConnection();
+
+    SplashScreen.hide();
   });
 
   return <Text>HOME SCREEN</Text>;
