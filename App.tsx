994b17154c009c3807b7b7efc22b6b0592b10b8e
Hitoishi Das <hitoishi.das@gmail.com>
2023-01-26 23:36:42 +0530

    Db init scripts wip

--- App.tsx
+++ App.tsx
@@ -10,13 +10,20 @@
 
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
-import React from 'react';
+import React, {useEffect} from 'react';
+import SplashScreen from 'react-native-splash-screen';
 
 import HomeScreen from './components/HomeScreen';
+import {dbOps} from './util/db';
 
 const Stack = createNativeStackNavigator();
 
 const App = (): JSX.Element => {
+  useEffect(() => {
+    dbOps.initiateDBConnection(); //open connection to SQLite database
+
+    SplashScreen.hide();
+  });
   return (
     <NavigationContainer>
       <Stack.Navigator initialRouteName="Home">
