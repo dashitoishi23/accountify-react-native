74ec20d8157cfc9f0fc9aee19551f89ee9507618
Hitoishi Das <hitoishi.das@gmail.com>
2023-05-28 23:04:53 +0530

    Fixed home screen condition

--- components/HomeScreen.tsx
+++ components/HomeScreen.tsx
@@ -1,6 +1,7 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 import React, {useEffect, useState} from 'react';
 import {View} from 'react-native';
+import {Text} from 'react-native-paper';
 import {dbOps} from '../util/db';
 import {getAccountifyUser, initDatabase} from '../util/db/init';
 import AffirmationButton from './common/AffirmationButton';
@@ -9,6 +10,7 @@ const HomeScreen: React.FC<{
   navigation: any;
 }> = ({navigation}) => {
   const [newUser, setIsNewUser] = useState(true);
+  const [isLoading, setIsLoading] = useState(true);
   const [accountifyUser, setAccountifyUser] = useState(null);
   const [date, setDate] = useState(new Date());
   useEffect(() => {
@@ -17,30 +19,39 @@ const HomeScreen: React.FC<{
       const dbConnection = dbOps.getDatabaseConnection();
       await initDatabase(dbConnection);
       const user = await getAccountifyUser(dbConnection);
-      console.log('User', user.item(0));
-      // if (user.length !== 0) {
-      //   setIsNewUser(true);
-      // } else {
-      //   setAccountifyUser(user.item(0));
-      // }
+      console.log('User', user.length);
+      if (user.length === 0) {
+        setIsNewUser(true);
+      } else {
+        setIsNewUser(false);
+        setAccountifyUser(user.item(0));
+      }
+      setIsLoading(false);
+      console.log('NEW', newUser);
     })();
-  });
+  }, [newUser]);
 
-  return newUser ? (
-    <View>
-      <AffirmationButton
-        onPressCallback={() => {
-          navigation.navigate('Settings');
-        }}
-        text="Add your details to get started"
-      />
-    </View>
+  return !isLoading ? (
+    newUser ? (
+      <View>
+        <AffirmationButton
+          onPressCallback={() => {
+            navigation.navigate('Settings');
+          }}
+          text="Add your details to get started"
+        />
+      </View>
+    ) : (
+      <View>
+        <AffirmationButton
+          onPressCallback={() => console.log('Pressed')}
+          text="Old User"
+        />
+      </View>
+    )
   ) : (
     <View>
-      <AffirmationButton
-        onPressCallback={() => console.log('Pressed')}
-        text="Old User"
-      />
+      <Text>Loading</Text>
     </View>
   );
 };
