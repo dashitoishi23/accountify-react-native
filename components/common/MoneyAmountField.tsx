61bc1f182f43b3325744e25ac60dc985676beeed
Hitoishi Das <hitoishi.das@gmail.com>
2024-10-02 16:45:07 +0530

    Fixed money field!

--- components/common/MoneyAmountField.tsx
+++ components/common/MoneyAmountField.tsx
@@ -1,8 +1,7 @@
 /* eslint-disable react-native/no-inline-styles */
 import React, {useEffect, useState} from 'react';
-import {View} from 'react-native';
+import {View, NativeModules, Platform } from 'react-native';
 import {Text, TextInput} from 'react-native-paper';
-import {currencyMasker} from '../../util/currencyMasker';
 import {getDimensions} from '../../util/getDimensions';
 
 const MoneyAmountField: React.FC<{
@@ -10,12 +9,30 @@ const MoneyAmountField: React.FC<{
   setAmount: Function;
   label: string;
 }> = ({amount, setAmount, label}) => {
-  const [fieldAmount, setFieldAmount] = useState('0');
+  const [fieldAmount, setFieldAmount] = useState('');
   const {windowWidth} = getDimensions();
 
+  const locale: string =
+  Platform.OS === 'ios'
+    ? NativeModules.SettingsManager.settings.AppleLocale
+    : NativeModules.I18nManager.localeIdentifier;
+
   useEffect(() => {
-    if (amount) setFieldAmount(amount);
+    if (amount) handleInputChange(amount);
   }, []);
+
+  const handleInputChange = (text: string) => {
+    // Remove all non-numeric characters except for the decimal point
+    const cleanedValue = text.replace(/[^0-9.]/g, '');
+
+    // Convert to float and format as currency
+    const formattedValue = cleanedValue
+      ? new Intl.NumberFormat(locale.replace('_', '-')).format(parseFloat(cleanedValue))
+      : '';
+
+    setFieldAmount(formattedValue);
+    setAmount(formattedValue);
+  };
   return (
     <View
       style={{
@@ -26,15 +43,11 @@ const MoneyAmountField: React.FC<{
         label="Amount"
         mode="outlined"
         keyboardType="decimal-pad"
-        value={currencyMasker(fieldAmount)}
+        value={fieldAmount}
         style={{
           fontSize: 20,
         }}
-        onChangeText={newAmount => {
-          console.log(newAmount, 'enterted');
-          setFieldAmount(newAmount ? currencyMasker(newAmount) : '0');
-          setAmount(currencyMasker(newAmount));
-        }}
+        onChangeText={handleInputChange}
       />
     </View>
   );
